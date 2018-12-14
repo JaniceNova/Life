60681f40ed90cc991ce411b50d435653e97940ba
JaniceNova <janicenovacek2016@gmail.com>
2018-12-14 09:40:50 -0800

    working on ideas

---
+++ assets/javascript/index.js
@@ -0,0 +1,5 @@
+$( ".square" ).click(function() {
+
+    $(".square").html("<input type='text' name='lastname' value='Mouse'><br><br><input type='submit' value='Submit'>")
+   console.log("hi")
+  });
