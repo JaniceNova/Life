ef623d836d4ab6f84a515b0e9dd15f6b350b281e
JaniceNova <janicenovacek2016@gmail.com>
2018-12-14 10:08:20 -0800

    work on page 2 boxes

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,5 +1,13 @@
 
-body.click(function() {
+var a = 0;
 
-    $("div").html("<input type='text' name='lastname' value='Mouse'><br><br><input type='submit' value='Submit'>")
+$(".square").click(function () {
+    if (a === 0){
+    $("#square1").html("<input type='text' name='lastname' value='Mouse'><br><br><input type='submit' value='Submit'>")
+     a=1
+                }   
+    else{
+     $("#square1").html("")
+      a=0
+             }   
   });
--- assets/javascript/page2.js
+++ assets/javascript/page2.js
@@ -1,6 +1,5 @@
 
-$( "#square1" ).click(function() {
+$( ".square" ).click(function() {
 
     $("#square1").html("<input type='text' name='lastname' value='Mouse'><br><br><input type='submit' value='Submit'>")
   });
-  
