caf19570903b3fd0f020933f2e148d896c2b448b
JaniceNova <janicenovacek2016@gmail.com>
2018-12-15 21:35:20 -0800

    ideas

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,13 +1,14 @@
 
-var a = 0;
+// var a = 0;
 
-$(".square").click(function () {
-    if (a === 0){
-    $("#square1").html("<input type='text' name='lastname' value='Mouse'><br><br><input type='submit' value='Submit'>")
-     a=1
-                }   
-    else{
-     $("#square1").html("")
-      a=0
-             }   
-  });
+// $(".square").click(function () {
+//     if (a === 0){
+//     $("#square1").html("<input type='text' name='lastname' value='Mouse'><br><br><input type='submit' value='Submit'>")
+//      a=1
+//                 }   
+//     else{
+//      $("#square1").html("")
+//       a=0
+//              }   
+//   });
+ 
